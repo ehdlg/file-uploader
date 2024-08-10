bc13c4de926df741b8e13c5c03da1326967d3aa3
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-08-10 09:16:49 +0200

    build: add react

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <h1 className='text-center text-3xl font-bold text-slate-800 underline mt-20'>
+      Welcome to file uploader
+    </h1>
+  );
+}
+
+export default App;
