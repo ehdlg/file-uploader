b9400e525d5c5292c1439ecd9217e4d9e6ec8dd3
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-08-18 11:43:03 +0200

    refactor(db): remove default export from pool and add typed query function

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -1,12 +1,15 @@
-import { Pool } from 'pg';
+import { Pool, QueryResultRow } from 'pg';
 import 'dotenv/config';
 
 const { DB_HOST, DB_USER, DB_PORT, DB_PWD, DB_NAME } = process.env;
 
-export default new Pool({
+const pool = new Pool({
   host: DB_HOST,
   user: DB_USER,
   password: DB_PWD,
   port: Number(DB_PORT),
   database: DB_NAME,
 });
+
+export const query = <T extends QueryResultRow>(text: string, params?: string[] | number[]) =>
+  pool.query<T>(text, params);
