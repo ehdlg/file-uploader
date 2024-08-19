edbab5fdd25c00eb8551f902e8031b60f6d933a4
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-08-19 13:06:16 +0200

    feat(middlewares): add notFound middleware

--- backend/src/middlewares/index.ts
+++ backend/src/middlewares/index.ts
@@ -1,8 +1,13 @@
-import { ErrorRequestHandler } from 'express';
+import { HttpError } from '../errors/HttpError';
+import { ErrorRequestHandler, RequestHandler } from 'express';
 
-export const handleError: ErrorRequestHandler = (error, _, res, __) => {
+export const handleError: ErrorRequestHandler = (error, _req, res, _next) => {
   const status = error.status || 500;
   const message = error.message || 'Something went wrong';
 
   res.status(status).json({ error: message });
+};
+
+export const notFound: RequestHandler = () => {
+  throw new HttpError({ message: 'Not found', status: 404 });
 };
