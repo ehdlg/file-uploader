517acf2d41eabcdbe472eceb19e1f7ff8615259b
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-08-22 13:53:56 +0200

    update getByCredentials and getByUsername functions names

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -7,13 +7,13 @@ export class User extends Base<IUser> {
     super(TABLES.USERS);
   }
 
-  async getByCredentials(email: string): Promise<IUser | null> {
+  async findByCredentials(email: string): Promise<IUser | null> {
     const { rows } = await this.query<IUser>('SELECT * FROM users WHERE email = $1', [email]);
 
     return rows[0] || null;
   }
 
-  async getByUsername(username: string): Promise<IUser | null> {
+  async findByUsername(username: string): Promise<IUser | null> {
     const { rows } = await this.query<IUser>('SELECT * FROM users WHERE username = $1', [username]);
 
     return rows[0] || null;
