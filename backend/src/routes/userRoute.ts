e1b143ea5c494ca01b7ce29299e582f584fc7c84
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-08-24 13:06:22 +0200

    feat(routes): implement folder route

--- backend/src/routes/userRoute.ts
+++ backend/src/routes/userRoute.ts
@@ -2,8 +2,11 @@ import { Router } from 'express';
 import UserController from '../controllers/UserController';
 import { validateData } from '../middlewares';
 import { userIdRule, userPostRules, userPutRules } from '../validation';
+import folderRoute from './folderRoute';
 
 const router = Router();
+
+router.use('/:userId/folders/', folderRoute);
 
 router.get('/:userId', userIdRule, validateData, UserController.getOne);
 
