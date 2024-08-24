8717753e309c4d7a2396bd9113c9dc4fc43b4e14
Enrique Hidalgo <121901959+ehdlg@users.noreply.github.com>
2024-08-24 13:05:35 +0200

    feat(models): add findAllByUser, findByFolderAndUserId and findByNameParentAndUser functions to Folder model

---
+++ backend/src/models/Folder.ts
@@ -0,0 +1,38 @@
+import Base from './Base';
+import { TABLES } from '../constants';
+import { UUID } from 'crypto';
+import { IFolder } from '../interfaces';
+
+export class Folder extends Base<IFolder> {
+  constructor() {
+    super(TABLES.FOLDERS);
+  }
+
+  async findAllByUser(userId: UUID) {
+    const sql = 'SELECT * FROM folders WHERE user_id = $1';
+
+    const { rows } = await this.query<IFolder>(sql, [userId]);
+
+    return rows;
+  }
+
+  async findByFolderAndUserId(folderId: UUID, userId: UUID) {
+    const sql = 'SELECT * FROM folders WHERE id = $1 AND user_id = $2';
+    const { rows } = await this.query<IFolder>(sql, [folderId, userId]);
+
+    return rows[0] || null;
+  }
+
+  async findByNameParentAndUser(folderName: string, userId: UUID, parentId: UUID | null) {
+    const sql = `
+     SELECT * FROM folders 
+     WHERE name = $1 AND user_id = $2 
+     AND (${parentId === null ? 'parent_id IS NULL' : 'parent_id = $3'})
+   `;
+    const params = parentId === null ? [folderName, userId] : [folderName, userId, parentId];
+
+    const { rows } = await this.query<IFolder>(sql, params);
+
+    return rows[0] || null;
+  }
+}
